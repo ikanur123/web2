af677720b86937b1efea06297d6a8f4f4e350937
ikanur123 <ikajoe0@gmail.com>
2020-03-09 23:12:22 +0700

    latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,14 @@
+<?php 
+$A = 123 ; // variable global 
+function Test() { 
+    $A = "Test"; // variable local 
+    echo"Nilai A dalam fungsi = $A \n"; 
+    } 
+Test(); 
+echo"Nilai A luar fungsi = $A \n"; 
+?>
+
+
+
+
+
