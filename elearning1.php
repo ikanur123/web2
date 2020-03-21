80ad590307e248bfad05d09f54ee5513be3b0437
ikanur123 <ikajoe0@gmail.com>
2020-03-21 17:21:34 +0700

    elearning1.php

---
+++ elearning1.php
@@ -0,0 +1,22 @@
+<?php
+
+$name = 'han solo';
+
+switch ($name) {
+    case 'yoda':
+        echo 'Do or do not there is no try.';
+        break;
+    case 'darth vader':
+        echo 'I am your Father.';
+        break;
+    case 'leia':
+        echo "Help me Obi-Wan Kenobi, you're my only hope.";
+        break;
+    case 'han solo':
+        echo 'I have a bad feeling about this.';
+        break;
+    default:
+        echo 'Who are you?';
+        break;
+}
+?>
