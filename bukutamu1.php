aab3bcd722a3dc838ecf95a327c2bd82d1e4f378
ikanur123 <ikajoe0@gmail.com>
2020-04-18 07:06:03 +0700

    bukutamu1

--- bukutamu1.php
+++ bukutamu1.php
@@ -17,6 +17,7 @@ Komentar:<textarea name="komentar" rows=4 cols=100></textarea>
 <p>
 <input type=submit value="Kirim Data" name="submit"> <input type=reset value="Hapus Data">
 </form>
+
 <?php
 $nama = $_GET['Nama'] ;
 $alamat = $_GET['Alamat'] ;
@@ -25,13 +26,14 @@ $kodepos = $_GET['KodePos'] ;
 $propinsi = $_GET['pilih'] ;
 $email = $_GET['E-Mail'] ;
 $komentar = $_GET['komentar'] ;
+
 if ($_GET['submit']) {
-    echo "Nama : ".$nama."<br>" ;
-    echo "Alamat : ".$alamat."<br>" ;
-    echo "Kota : ".$kota."<br>" ;
-    echo "Kode Pos : ".$kodepos."<br>" ;
-    echo "Propinsi : ".$propinsi."<br>" ;
-    echo "Email : ".$email."<br>" ;
-    echo "Komentar : ".$komentar."<br>" ;
-    }
+echo "Nama : ".$nama."<br>" ;
+echo "Alamat : ".$alamat."<br>" ;
+echo "Kota : ".$kota."<br>" ;
+echo "Kode Pos : ".$kodepos."<br>" ;
+echo "Propinsi : ".$propinsi."<br>" ;
+echo "Email : ".$email."<br>" ;
+echo "Komentar : ".$komentar."<br>" ;
+}
 ?>
