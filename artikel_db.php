4dc47afe52586fb59327356f131a9eeae048c899
ikanur123 <61970656+ikanur123@users.noreply.github.com>
2020-06-13 11:16:37 +0700

    Delete artikel_db.php

--- artikel_db.php
+++
@@ -1,8 +0,0 @@
-<? 
-mysql_connect("localhost","root",""); 
-$dbname="artikel_db"; 
-$cek=mysql_query("CREATE DATABASE $dbname") or die("Couldn't Create Database: $dbname");   
-if($cek){   
-    echo "Database $dbname berhasil dibuat"; 
-} 
-?> 
